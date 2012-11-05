6c690753281c8e4205b40237201e924704695bdd
Catsquotl <catsquotl@gmail.com>
2012-11-05 18:55:18 +0100

    Update w1e3.rb

--- w1e3.rb
+++ w1e3.rb
@@ -1,7 +1,7 @@
 Seccond = 1.0
-Minute = 60 * Seccond
-Hour = 60 * Minute
-Day = 24 * Hour
-Year = 365 * Day
+minute = 60 * Seccond
+hour = 60 * minute
+day = 24 * hour
+year = 365 * day
 
 puts "if I am 979000000 secconds old. I am #{"%.2f" % (979000000 / Year)} Years old"
