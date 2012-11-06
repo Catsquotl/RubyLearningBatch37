53396b71d1267d4f935acb787a510b24fa2653f6
Catsquotl <catsquotl@gmail.com>
2012-11-06 18:22:04 +0100

    added weekly directories

---
+++ W1/w1e3.rb
@@ -0,0 +1,7 @@
+Second = 1.0
+minute = 60 * Second
+hour = 60 * minute
+day = 24 * hour
+year = 365 * day
+
+puts "if I am 979000000 secconds old. I am " + format("%.2f",979000000 / year) + " Years old"
--- w1e3.rb
+++
@@ -1,7 +0,0 @@
-Seccond = 1.0
-minute = 60 * Seccond
-hour = 60 * minute
-day = 24 * hour
-year = 365 * day
-
-puts "if I am 979000000 secconds old. I am #{"%.2f" % (979000000 / year)} Years old"
