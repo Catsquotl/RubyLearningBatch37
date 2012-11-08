d30b4a90d30d2509920a43cd49ea467f0c407035
Catsquotl <catsquotl@gmail.com>
2012-11-08 07:43:25 +0100

    made it abit better

--- W1/w1c1.rb
+++ W1/w1c1.rb
@@ -1,13 +1,18 @@
 Second = 1.0
-minute = 60 * Second
-hour = 60 * minute
-day = 24 * hour
-month = 30 * day
-year = 365 * day
+@minute = 60 * Second
+@hour   = 60 * @minute
+@day    = 24 * @hour
+@month  = 30 * @day
+@year   = 365 * @day
 
-sec = Array.new [979000000, 2158493738, 246144023, 1270166272, 1025600095]
-sec.each do |time|
-  puts "I am #{(time / year).to_i} years and #{((time % year)/month).to_i} months old"
+def convert age_in_seconds
+ "I am #{"%2d" % (age_in_seconds / @year).to_i} years and #{"%2d" % (age_in_seconds % @year/@month).to_i} months old"
 end
+
+ages_in_seconds = [979000000, 2158493738, 246144023, 1270166272, 1025600095]
+ages_in_seconds.each do |time|
+   puts convert time
+end
+
 
 
