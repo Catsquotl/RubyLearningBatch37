96becf89c04d0cfef776e899fa170ae22cc69fb7
Catsquotl <catsquotl@gmail.com>
2012-11-09 14:08:54 +0100

    think I got w1c3 up to spec

---
+++ W1/doctest1.rb
@@ -0,0 +1,5 @@
+=begin
+doctest: "%1$*2$s %2$d %1$s" % ["hello", 8]
+>> "%1$*2$s %2$d %1$s" % ["hello", -8]
+=> "   hello 8 hello"
+=end
--- W1/w1c3.rb
+++ W1/w1c3.rb
@@ -1,28 +1,31 @@
 #Multiplication tables
 
 def multiplication_table num,heading="",decoration=false
+
   table = Array.new
   (1..num).each do |x|
     (1..num).each do |y|
      table.push(x * y)
     end
   end
+
   column_width = table.last.to_s.size
   str = ""
+
   until table.empty?
     table.slice!(0...num).each do |cell|
-      str << cell.to_s.rjust(column_width + 1)
+      str << cell.to_s.rjust(column_width) + ' '
     end
     str << "\n"
   end
+
   output = "#{heading.center(num * (column_width + 1))}\n"
+
   if decoration
-    output << "#" * (num * ((column_width + 1))+2) + "\n"
+    dec = '=' * (num  * (column_width + 1 )) + "\n"
+    output << dec << str << dec
+  else
+    output << str
   end
-  output << str
-
-output
 end
-
-puts multiplication_table 9,"times 9"
-puts multiplication_table 20, "Times 20",true
+puts multiplication_table 20, 'Table times to 9', true
