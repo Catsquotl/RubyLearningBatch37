39441140094d0bc44652b126db4da202b2683131
catsquotl <catsquotl@gmail.com>
2012-11-15 20:18:33 +0100

    from ide

--- w2/prompt.rb
+++ w2/prompt.rb
@@ -1,4 +1,13 @@
-def prompt prompt
+=begin
+doctest: Test the prompt method by providing the answer
+>> prompt "What is your name", "Zaphod"
+=> "Zaphod\n"
+doctest: Test the prompt by providing an answer and telling it I want the result chomped
+>> prompt "What is your name", "Zaphod", true
+=> "Zaphod"
+=end
+
+def prompt *prompt
   print "#{prompt}:> "
-  user_input = gets.chomp!
+  gets.chomp!
 end
