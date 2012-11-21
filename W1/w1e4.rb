c17fb534fe20389e1b265483b244d69642a2f30e
Catsquotl <catsquotl@gmail.com>
2012-11-21 11:11:43 +0100

    removed some

--- W1/w1e4.rb
+++
@@ -1,9 +0,0 @@
-Second = 1.0
-minute = 60 * Second
-hour = 60 * minute
-day = 24 * hour
-year = 365 * day
-
-minutes_in_a_year = year / minute
-
-puts "A year has #{minutes_in_a_year.to_i} minutes"
--- W1/w1e5.rb
+++
@@ -1,6 +0,0 @@
-my_string = 'Hello Ruby World'
-
-def my_string
-  'Hello World'
-end
-puts my_string
