8938425480fd0f9cf0695e97b8ffdc2d5c82320a
Catsquotl <catsquotl@gmail.com>
2012-11-23 15:46:46 +0100

    getting in some doctestcode...w1e6.rb

--- w1/w1e6.rb
+++ w1/w1e6.rb
@@ -5,6 +5,14 @@ def convert(degrees_fahrenheit)
   celsius = (degrees_fahrenheit.to_f - 32) * 5/9
 end
 
+=begin
+doctest: testing the output of the convert method
+>> convert(100)
+=> 37.77777777777778
+=end
+if __FILE__==$0
 puts "Please insert degrees in fahrenheit to be converted to degrees in Celsius"
 degrees_in_fahrenheit = gets.chomp
 puts "#{degrees_in_fahrenheit} degrees Fahrenheit is #{"%.2f" % convert(degrees_in_fahrenheit)} degrees Celsius"
+
+end
