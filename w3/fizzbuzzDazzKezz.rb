3f290703e9226ddce17c2d4c303ba52982816cda
catsquotl <catsquotl@gmail.com>
2012-11-25 23:19:21 +0100

    fizzbuzzdazzkezz.rb final for now

--- w3/fizzbuzzDazzKezz.rb
+++ w3/fizzbuzzDazzKezz.rb
@@ -11,7 +11,7 @@ def fizzbuzz(range, rules = {3 => 'Fizz',5 => 'Buzz'})
 end
 =begin
   doctest : given a range, a hash where {divisor => keyword} str should be added with keyword if number mod divisor == 0
->> fizzbuzz((1..15),{3 => 'Fizz',5 => 'Buzz',7 => 'Dazz', 11 => 'Kezz'})
+>> fizzbuzz((-15..15),{3 => 'Fizz',5 => 'Buzz',7 => 'Dazz', 11 => 'Kezz'})
 => ["*FizzBuzz*", "*Dazz*", -13, "*Fizz*", "*Kezz*", "*Buzz*", "*Fizz*", -8, "*Dazz*", "*Fizz*", "*Buzz*", -4, "*Fizz*", -2, -1, "*FizzBuzzDazzKezz*", 1, 2, "*Fizz*", 4, "*Buzz*", "*Fizz*", "*Dazz*", 8, "*Fizz*", "*Buzz*", "*Kezz*", "*Fizz*", 13, "*Dazz*", "*FizzBuzz*"]
 =end
 if __FILE__ == $0
