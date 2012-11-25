7d7ee84ba0a1c3ec7fc32b5716554de0dcc7b617
catsquotl <catsquotl@gmail.com>
2012-11-25 18:30:52 +0100

    updated fizzbuzzDazzKezz.rb

--- w3/fizzbuzzDazzKezz.rb
+++ w3/fizzbuzzDazzKezz.rb
@@ -1,40 +1,20 @@
 def fizzbuzz(range, hash)
-  str = ''
+  output = ''
   range.each do |num|
+  	str = ''
     hash.each do |divisor,word|
       str << word if num % divisor.to_i == 0
     end
-
-    if str.empty?
-      puts num 
-    else
-      puts "*#{str}*"
-    end 
-    str = '' 
+    str.empty? ? output << "#{num}\n" : output << "*#{str}*\n"
   end
+  output
 end
 =begin
-  doctest : given a number or range, a divisor and a keyword str should be added with keyword if number mod divisor == 0
+  doctest : given a range, a hash where {divisor => keyword} str should be added with keyword if number mod divisor == 0
 >> fizzbuzz((1..15),{3 => 'Fizz',5 => 'Buzz',7 => 'Dazz', 11 => 'Kezz'})
-=>
-1
-2
-*Fizz*
-4
-*Buzz*
-*Fizz*
-*Dazz*
-8
-*Fizz*
-*Buzz*
-*Kezz*
-*Fizz*
-13
-*Dazz*
-*FizzBuzz*
-
+=> "1\n2\n*Fizz*\n4\n*Buzz*\n*Fizz*\n*Dazz*\n8\n*Fizz*\n*Buzz*\n*Kezz*\n*Fizz*\n13\n*Dazz*\n*FizzBuzz*\n"
 =end
 if __FILE__ == $0
 has = {3 => 'Fizz',5 => 'Buzz',7 => 'Dazz', 11 => 'Kezz'}
-fizzbuzz((1..15),has)
+puts fizzbuzz((-15..15),has)
 end
