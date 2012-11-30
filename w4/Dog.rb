e1eee838c3fef274e4353229fe54106be4008da8
catsquotl <catsquotl@gmail.com>
2012-11-30 06:54:30 +0100

    working dog here

---
+++ w4/Dog.rb
@@ -0,0 +1,41 @@
+class Dog
+
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  
+  def bark
+    "woof woof"
+  end
+
+  def eat
+    "#{@name} is eating"
+  end
+
+  def chase_cat
+    "#{@name} is chasing a cat"
+  end
+
+  def teach_trick(sym,&block)  
+    self.define_singleton_method(sym,&block)
+   # This works too...
+   #   define_singleton_method(sym) do
+   #   instance_eval &block
+   #  end
+  end  
+
+  def method_missing(m,*args,&block)
+    puts "#{@name} doesn't know how to #{m}"
+  end
+
+end
+d = Dog.new('Leo')
+puts d.name
+puts d.bark
+puts d.eat
+puts d.chase_cat
+d.teach_trick(:hi){ "#{@name} is saying hello" } 
+puts d.hi
+d.doanything
