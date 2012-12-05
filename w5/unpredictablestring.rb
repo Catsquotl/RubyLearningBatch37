e61aa8b3456b9196f6e412f0c66ca139237f2337
catsquotl <catsquotl@gmail.com>
2012-12-05 06:17:36 +0100

    finaly an unpredictable string(ish)

--- w5/unpredictablestring.rb
+++ w5/unpredictablestring.rb
@@ -1,13 +1,20 @@
-class Unpredictablestring < String
-  def scramble
-    arr = []
-    self.each_char do |c|
-      arr << c
+  class String
+    def scramble! 
+      arr = []
+      self.each_char do |c|
+	arr << c
+      end
+      self.replace(arr.shuffle!.join)
     end
-    str =  arr.shuffle!.join
+  end
+
+class Unpredictablestring < String
+   def initialize str
+     super str.scramble!
   end
 end
-
 str = Unpredictablestring.new("It was a dark and stormy night.") 
-puts str.scramble
- 
+puts str
+puts str.scramble!
+puts str
+puts "#{str}"
