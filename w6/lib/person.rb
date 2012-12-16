49dd6e21b509197c903bd420d5b7edbba311affa
catsquotl <catsquotl@catsquotl-vm.(none)>
2012-12-16 15:43:28 +0100

    passing balance test

--- w6/lib/person.rb
+++ w6/lib/person.rb
@@ -3,6 +3,26 @@ class Person
 
   def initialize name, balance = 0.0
     @name = name
-    @balance = balance.to_f
+      if balance >= 0
+        @balance = balance.to_f
+      else   
+        raise ArgumentError,"Must have initial balance >=0"
+      end
   end
+
+  def balance
+    @balance
+  end
+
+  private 
+  def balance= bal
+    balance += bal
+  end
+if $0 == __FILE__
+p1 = Person.new("name",0)
+p2 = Person.new("fail",-1)
+
+p p1
+p p2
+end
 end
--- w6/spec/person_spec.rb
+++ w6/spec/person_spec.rb
@@ -1,16 +1,24 @@
 require "minitest/autorun"
 require "minitest/reporters"
-MiniTest::Reporters.use! Minitest::Reporters::SpecReporter.new
+MiniTest::Reporters.use! MiniTest::Reporters::SpecReporter.new
 
 require_relative "../lib/person.rb"
 
 describe Person do
   before do
-    @person = Person.new("Name",0.0)
+      @person = Person.new("Name",(0.0))
   end
-
-  it "must have a name" do
-    @person.name.must_equal "Name"
+  describe Person,"must have a name" do
+    it "must have a name" do
+      @person.name.must_equal "Name"
+    end
+    it "must have an initial balance >= 0" do
+      @person.balance.must_equal  0.0
+      initial_balance = Proc.new do
+      p = Person.new("Name",-1)
+      end
+      initial_balance.must_raise ArgumentError
+    end
   end
 
 end
