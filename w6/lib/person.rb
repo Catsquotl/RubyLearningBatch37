ca814cd62ba3408b19ace2645ea6ca7dcd941a8a
Catsquotl <catsquotl@gmail.com>
2012-12-19 09:51:37 +0100

    person test and rb

--- w6/lib/person.rb
+++ w6/lib/person.rb
@@ -3,26 +3,31 @@ class Person
 
   def initialize name, balance = 0.0
     @name = name
-      if balance >= 0
-        @balance = balance.to_f
-      else   
-        raise ArgumentError,"Must have initial balance >=0"
-      end
+    if balance >= 0
+      @balance = balance.to_f
+    else   
+      raise ArgumentError,"Must have initial balance >=0"
+    end
   end
 
   def balance
     @balance
   end
 
-  private 
-  def balance= bal
-    balance += bal
+  def add_balance add
+    @balance += add
   end
-if $0 == __FILE__
-p1 = Person.new("name",0)
-p2 = Person.new("fail",-1)
 
-p p1
-p p2
+  def substract_from_balance min
+    @balance -= min
+  end
+
 end
+
+if $0 == __FILE__
+  p1 = Person.new("name",0)
+  p2 = Person.new("fail",-1)
+
+  p p1
+  p p2
 end
--- w6/spec/person_spec.rb
+++ w6/spec/person_spec.rb
@@ -6,7 +6,7 @@ require_relative "../lib/person.rb"
 
 describe Person do
   before do
-      @person = Person.new("Name",(0.0))
+    @person = Person.new("Name",(0.0))
   end
   describe Person,"must have a name" do
     it "must have a name" do
@@ -16,11 +16,16 @@ describe Person do
       @person.balance.must_equal  0.0
     end
     it "must raise an argument error if initial balance is less than 0" do
-      initial_balance = Proc.new do
-      p = Person.new("Name",-1)
+      assert_raises(ArgumentError)do
+        Person.new("Name",-1)
       end
-      initial_balance.must_raise ArgumentError
     end
   end
-
+  it "Must be ables to add to balance" do
+    @person.add_balance(15).must_equal 15.0
+  end
+  it "Must be able to substract from balance" do
+    @person.substract_from_balance(15).must_equal(-15.0)
+  end
 end
+
