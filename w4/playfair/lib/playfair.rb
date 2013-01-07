c3b90c68b734375dddc5133e6e82137ebf02e708
catsquotl <catsquotl@gmail.com>
2013-01-07 15:05:57 +0100

    part of step 2

--- w4/playfair/lib/playfair.rb
+++ w4/playfair/lib/playfair.rb
@@ -1,31 +1,61 @@
 class Playfair
-  @@init_grid = ('A'...'J').to_a + ('K'..'Z').to_a
-  attr_reader :grid
+  @@BASE_GRID = ('A'...'J').to_a + ('K'..'Z').to_a
 
-  def initialize key
-    @key = key
-    setup
+  attr_reader :text, :grid
+
+  def initialize key, text
+    @token = 'X'
+    @index = nil
+    @key = key.upcase
+    @orig_text = text.upcase
+    make_key_grid
+    format_text
   end
 
-  def setup
-    @grid = []
+  def update_token
+    if @token.eql? 'X'
+      @token = 'Z'
+    else
+      @token = 'X'
+    end
+  end
 
-    @key.chars do |ch|
-      if ch == ('j' || 'J')
-	ch = 'I'
-      end
+  def update_index
+    @index = (@text =~ /(.)\1/)
+  end
+
+  def prepare txt
+    text = ''
+    txt.chars do |ch|
       if /[[:alpha:]]/ =~ ch
-	@grid.push ch.upcase
-      end
-
-    end 
-    @grid += @@init_grid
-    @grid.uniq!
+	if ch.eql? 'J'
+	  text.concat 'I'
+	else
+	  text.concat ch	
+	end
+      end 
+    end
+    text.split(//)
   end 
 
+  def make_key_grid
+    @grid =(self.prepare @key)
+    @grid += @@BASE_GRID
+    @grid.uniq!
+  end
+
+  def format_text
+    @text = (self.prepare @orig_text).join
+    update_index
+    while @index
+      @text.insert(@index + 1, @token)
+      update_index
+      update_token
+    end
+  end
 end
 
-if __FILE__==$0
-  p = Playfair.new 'het is mij een waar genoegen'
-  p p
-end 
+if $0 == __FILE__
+  ps = Playfair.new('I Love Ruby', 'Heel veel raar gedoe')
+  p ps
+end
--- w4/playfair/spec/playfair_spec.rb
+++ w4/playfair/spec/playfair_spec.rb
@@ -1,14 +1,26 @@
 require 'minitest/autorun'
-require 'minitest/spec'
+require 'minitest/colorize'
 require_relative '../lib/playfair'
 
 
 describe Playfair do
-  let (:ilr){Playfair.new('I love Ruby')}
-  let (:pf){Playfair.new('Playfair example')}
+  ilr_key = 'I love Ruby'
+  pf_key = 'Playfair example'
+  text = 'Hide the gold in the tree stump'
+  let (:ilr){Playfair.new("#{ilr_key}","#{text}")}
+  let (:pf){Playfair.new("#{pf_key}","#{text}")}
 
+  describe 'Making the grid' do
     it 'must create 5 by 5 grid' do
       ilr.grid.must_equal %w[I L O V E R U B Y A C D F G H K M N P Q S T W X Z]
       pf.grid.must_equal %w[P L A Y F I R E X M B C D G H K N O Q S T U V W Z]
     end
+
+    describe "Making the text" do
+      it "must change the format the initial text for processing" do
+	expected = 'HIDETHEGOLDINTHETREXESTUMP'
+	ilr.text.must_equal expected
+      end
+    end
   end
+end
