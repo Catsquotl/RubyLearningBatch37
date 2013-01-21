402b662d39e792f626e3c1a7254329479f61304e
catsquotl <catsquotl@gmail.com>
2013-01-21 06:56:12 +0100

    another try

--- w4/playfair/lib/playfair.rb
+++ w4/playfair/lib/playfair.rb
@@ -3,14 +3,57 @@ class Playfair
 
   attr_reader :text, :grid
 
-  def initialize key, text
+  def initialize key
+    make_key_grid key
+  end
+
+  def make_key_grid key
+    @key = prepare key
+    @grid = @key.split(//) + BASE_GRID
+    @grid.uniq!
+  end
+
+  def pair_text tmp
+    tmp_ar = tmp.split(//)
+    @pairs = []
+    @pairs.push tmp_ar.shift(2) until tmp_ar.empty?
+    tokenize
+  end
+  
+  def tokenize  
+  @pairs.each do |pair|
+      if need_token? pair
+	insert_token pair
+	break
+      end
+    end
+  end
+
+  def prepare txt
+    txt.upcase!
+    text = ''
+    txt.chars do |ch| 
+      if /[[:alpha:]]/ =~ ch
+	ch = 'I' if ch.eql? 'J'
+	text.concat ch	
+      end
+    end
+    text
+  end 
+
+  def encrypt message
+    @text = prepare message
+    pair_text @text 
+  end
+
+  def need_token? pair
+    pair[0] == pair[1]? true : false
+  end
+
+  def insert_token pair
     @token ||= 'X'
-    @index = nil
-    @key = key
-    @orig_text = text
-    make_key_grid
-    format_text text.upcase
-    finalize @text
+    pair.insert(1, @token) 
+    update_token
   end
 
   def update_token
@@ -21,46 +64,13 @@ class Playfair
     end
   end
 
-  def insert_token txt
-    txt.each do |pair|
-      if pair[0].eql? pair[1]
-	pair.insert(1, @token)
-	update_token
-	tmp = txt.join
-	@text = tmp
-	self.format_text tmp
-      end
-    end
-  end
-
-  def prepare txt
-    @text = ''
-    txt.chars do |ch| 
-      if /[[:alpha:]]/ =~ ch
-	ch = 'I' if ch.eql? 'J'
-	@text.concat ch	
-      end
-    end
-    @text.split(//)
-  end 
-
-  def make_key_grid
-    @grid = (self.prepare @key.upcase) + BASE_GRID
-    @grid.uniq!
-  end
-
-  def format_text text
-    txt = []
-    tmp = (self.prepare text)
-    txt.push tmp.shift(2) until tmp.empty?
-    insert_token txt
-  end
-
   def finalize txt
     @text.concat 'X' if txt.size.odd?
   end
-end 
+
+end
 if $0 == __FILE__
-  ps = Playfair.new('I Love Ruby', 'ruuuuby Loove')
+  ps = Playfair.new('I Lovej Ruby')
+  ps.encrypt('ruuuuby Loove..!@#')
   p ps
 end
